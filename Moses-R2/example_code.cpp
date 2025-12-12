c92aa053bd30243254e1e962a31324514be70146
AuGiE <96394347+BigCart69421@users.noreply.github.com>
2025-12-12 13:41:01 +1100

    General Updates

--- Moses-R2/example_code.cpp
+++ Moses-R2/example_code.cpp
@@ -1,6 +1,6 @@
 // Moses R2 code example
 // Thank you to Arduino Documentation and Adafruit, as well as numerous open-source contributors fot the libraries
-// ad kowledge that enabled me to write this code
+// and kowledge that enabled me to write this code
 
 
 #include <Arduino.h>
@@ -10,6 +10,14 @@
 
 float ground = 0;
 float zeroed = 0;
+float altitude = 0;
+float oldAltitude = 0;
+float velocity = 0;
+float change;
+
+unsigned long oldTime = 0;
+unsigned long currentTime = 0;
+unsigned long deltaTime = 0;
 
 int CSpin = 4;
 
@@ -27,5 +35,19 @@ void setup() {
     }
     Serial.println("Baro init success!");
     ground = bmp.readAltitude();
-    Serial.print
+    Serial.print("Computer Zeroed! Ground Altitude: ");
+    Serial.print(ground);
+    Serial.println("m");
+    currentTime = millis();
+}
+
+void loop() {
+    oldAltitude = altitude;
+    currentTime = millis();
+    altitude = bmp.readAltitude() - ground;
+    change = altitude - oldAltitude;
+    deltaTime = (currentTime - oldTime) / 1000;
+    velocity = change / deltaTime;
+    if (velocity > 1) {}
+
 }
